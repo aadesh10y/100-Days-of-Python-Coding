3479623c5f745cf19799e9f4f23073afc4a138cd
aadesh10y <138278725+aadesh10y@users.noreply.github.com>
2025-06-03 08:30:22 +0545

    Add files via upload

---
+++ DAY 1/String_manipulation.py
@@ -0,0 +1,4 @@
+print("Hello Programmers \nHello Programmers \n ")
+
+print("Aadesh"+" Paudel") #Concatination
+print("Aadesh" + " " + "Paudel") 
---
+++ DAY 1/variables.py
@@ -0,0 +1,6 @@
+print(len(input("What is your name? ")))
+
+name = "Sujal"
+print(name)
+print(len(name)) #len function to calculate the length of string
+
